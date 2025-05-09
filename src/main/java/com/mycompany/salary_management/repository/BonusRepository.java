c906b75e2f3debae9aa1457ccabbcd9676a67f0a
Herinjaka-Antonio RAKOTOARIVONY <herinjaka-antonio.rakotoarivony@renault.com>
2025-05-09 14:52:04 +0200

    Fix build

--- src/main/java/com/mycompany/salary_management/repository/BonusRepository.java
+++ src/main/java/com/mycompany/salary_management/repository/BonusRepository.java
@@ -2,8 +2,12 @@ package com.mycompany.salary_management.repository;
 
 import com.mycompany.salary_management.entity.Bonus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface BonusRepository extends JpaRepository<Bonus, Long> {
-    List<Bonus> findByPayrollId(Long payrollId);
+    @Query("SELECT b FROM Bonus b JOIN b.payrolls p WHERE p.id = :payrollId")
+    List<Bonus> findByPayrollId(@Param("payrollId") Long payrollId);
 }
--- src/main/java/com/mycompany/salary_management/repository/DeductionRepository.java
+++ src/main/java/com/mycompany/salary_management/repository/DeductionRepository.java
@@ -2,8 +2,12 @@ package com.mycompany.salary_management.repository;
 
 import com.mycompany.salary_management.entity.Deduction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface DeductionRepository extends JpaRepository<Deduction, Long> {
-    List<Deduction> findByPayrollId(Long payrollId);
+    @Query("SELECT d FROM Deduction d JOIN d.payrolls p WHERE p.id = :payrollId")
+    List<Deduction> findByPayrollId(@Param("payrollId") Long payrollId);
 }
